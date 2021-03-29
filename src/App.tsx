4b84d4eff874f4d245b792747100b806505a984f
Femi Omotayo <omotayooluwafemi66@gmail.com>
2021-03-29 18:21:39 +0100

    soltra admin dashboard project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SOLTRA ADMIN DASHBOARD! LET'S DO THIS!!!</h1>
+    </div>
+  );
+}
+
+export default App;
+
